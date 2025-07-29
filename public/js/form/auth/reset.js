23975630f09560b927ac592763372a94c9c1965d
Roltan <KhalikoR@yandex.ru>
2025-07-29 16:26:14 +0500

    починил смену пароля

--- public/js/form/auth/reset.js
+++ public/js/form/auth/reset.js
@@ -1,26 +1,32 @@
-import {modal, message} from "../../util/modal.js";
-import {load} from "../../util/spiner.js";
+import { modal, message } from "../../util/modal.js";
+import { load } from "../../util/spiner.js";
 
 document
-    .querySelector('#reset-form')
-    .addEventListener('submit', function (event) {
+    .querySelector("#reset-form")
+    .addEventListener("submit", function (event) {
         event.preventDefault();
 
-        fetch('/auth/api/password/change', {
-            method: 'post',
+        const url = new URL(window.location.href);
+        const token = url.searchParams.get("token");
+
+        fetch("/auth/api/password/change", {
+            method: "post",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
                 email: this.querySelector('input[name="email"]').value,
+                token: token,
                 password: this.querySelector('input[name="password"]').value,
-                password_confirmation: this.querySelector('input[name="password_confirmation"]').value
-            })
+                password_confirmation: this.querySelector(
+                    'input[name="password_confirmation"]'
+                ).value,
+            }),
         })
             .then((response) => response.json())
             .then((data) => {
                 if (data.status) {
-                    window.location = '/login';
+                    window.location = "/login";
                 } else {
                     modal(data.message);
                 }
